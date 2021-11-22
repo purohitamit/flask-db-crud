e35d0dfcf2a3fc5144e4037281cb56fa70f27201
Ubuntu <azureuser@myVM.5qh0wf5nsbruhlksmk54necr3c.zx.internal.cloudapp.net>
2021-11-22 17:19:18 +0000

    pushing to github

---
+++ application/routes.py
@@ -0,0 +1,44 @@
+from application import app, db
+from application.models import Games
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/add')
+def add():
+    new_game = Games(name="New Game")
+    db.session.add(new_game)
+    db.session.commit()
+    return "Added new game to database"
+
+@app.route('/read')
+def read():
+    all_games = Games.query.all()
+    games_string = ""
+    for game in all_games:
+        games_string += "<br>"+ game.name
+    return games_string
+
+@app.route('/update/<name>')
+def update(name):
+    first_game = Games.query.first()
+    first_game.name = name
+    db.session.commit()
+    return first_game.name
+
+@app.route('/count')
+def count():
+    number_of_games = Games.query.count()
+    return str(f'You have {number_of_games} games in the database')
+
+@app.route('/delete')
+def delete():
+    game_to_delete = Games.query.first()
+    db.session.delete(game_to_delete)
+    db.session.commit()
+    number_of_games_now = Games.query.count()
+    return str(f'You have deleted 1 game and now you have {number_of_games_now} remaining games in the database')
